e1b11ebf177eb74c8d88c1d1c0bf308ec81ed697
Igor Camilo <igor.rcamilo@gmail.com>
2019-10-09 22:48:02 +0200

    Animations!

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -17,6 +17,8 @@ public class RubyController : MonoBehaviour
     bool isInvincible;
     float invincibleTimer;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
 
     // Start is called before the first frame update
     void Start()
@@ -25,13 +27,26 @@ public class RubyController : MonoBehaviour
         var input = GetComponent<PlayerInput>();
         moveAction = input.actions.FindAction("Move");
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 move = moveAction.ReadValue<Vector2>();
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         Vector2 position = rigidbody.position;
-        position += moveSpeed * Time.deltaTime * moveAction.ReadValue<Vector2>();
+        position += moveSpeed * Time.deltaTime * move;
         rigidbody.MovePosition(position);
 
         if (isInvincible)
@@ -51,6 +66,7 @@ public class RubyController : MonoBehaviour
             
             isInvincible = true;
             invincibleTimer = timeInvincible;
+            animator.SetTrigger("Hit");
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
